45e79cb8d18e72c9363c90429003ac50b38e1140
saeed <saeed.bayati89@gmail.com>
2025-02-03 01:15:13 +0330

    5 - Layouts & Slots

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<header>
+    <nav>
+        <h1>Ninja network</h1>
+        <a href="/ninjas">All ninjas</a>
+        <a href="/ninjas/create">Creat new ninjas</a>
+    </nav>
+</header>
+<main class="container">
+{{$sl}}
+</main>
+</body>
+</html>
